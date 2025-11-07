88f03c27110b37fdcc9f14a7dd19cceaf392b989
unknown <nazwa@TomekJastrzebski.ace.local>
2025-11-07 18:04:43 +0100

    posprzatanie po bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client'; // Poprawne zaimportowanie createRoot
-import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'; // własne style powinny być po Bootstrapie, aby nadpisać jego reguły
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
